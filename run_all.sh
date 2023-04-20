bbbecd317aa6a8f1a4d95a2de40d6df09835d4e5
Abdullah <s-abdallah@zewailcity.edu.eg>
2023-04-21 01:32:05 +0200

    first commit 3

--- run_all.sh
+++ run_all.sh
@@ -1,5 +1,5 @@
 git add .
-git commit -m "first commit "
+git commit -m "first commit 3"
 git branch -M main
 # git remote add origin https://github.com/Abdullah-Eisa/try_git_cmds.git
 git push -u origin main
